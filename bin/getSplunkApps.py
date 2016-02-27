13ec33e53bb51ca6db4eb09c2faa3b5123387f16
Chhean Saur <chhean@saurinc.com>
2016-02-26 18:27:03 -0800

    add ability to automatically paginate through entire list of apps without needing to update the total

--- bin/getSplunkApps.py
+++ bin/getSplunkApps.py
@@ -4,25 +4,46 @@
 import json
 import urllib2
 
-### Open the URL and the screen name
-base_url = "https://splunkbase.splunk.com/api/v0/apps/?order=latest&limit=100&offset="
+# function that does an http get to download the list of apps that returned in JSON format
+def get_apps(limit, offset):
+  ### Base URL to download list of apps
+  base_url = "https://splunkbase.splunk.com/api/v0/apps/?order=latest&limit="+ str(limit) + "&offset="
 
-### Iterate through all pages to download the json containing a list of apps.  
-### This will need to be updated after there more than 2,100 Apps.  
-### The searches in the dashboard are also tuned to this number.
-
-for i, offset in enumerate(range(0, 2000, 100)):
+  # build the url to download the list of apps
   url = base_url + str(offset)
 
   ### This takes a python object and dumps it to a string which is a JSON representation of that object
   data = json.load(urllib2.urlopen(url))
 
-  apps = data['apps']
+  # return the json data
+  return data
 
+
+def main():
+  # download initial list of the apps
+  data = get_apps(100, 0)
+
+  # get the total number of apps
+  total = data['total']
+
+  apps = data['apps']
   ### Print the result
   for app in list(apps):
     jsonText = json.dumps(app, indent=2)
     print jsonText + "\n"
 
+  ### Iterate through all pages to download the json containing a list of apps.
+  ### continue downloading the remaining list of apps
+  for i, offset in enumerate(range(100, total, 100)):
+    # download the list of apps
+    data = get_apps(100, offset)
+
+    apps = data['apps']
+
+    ### Print the result
+    for app in list(apps):
+      jsonText = json.dumps(app, indent=2)
+      print jsonText + "\n"
 
 
+if __name__ == "__main__": main()
